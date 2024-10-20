f366a0f434e1341e17c78c3f22f74422bb73cf7a
0xn4utilus <gyanendrabanjare8@gmail.com>
2024-10-20 08:57:43 -0700

    update zip
    Signed-off-by: 0xn4utilus <gyanendrabanjare8@gmail.com>

--- pint_auction/src/app/host/page.js
+++ pint_auction/src/app/host/page.js
@@ -72,7 +72,7 @@ export default function Host() {
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href=""
+            href="https://youtu.be/eB7SaMKbE9A"
             target="_blank"
             rel="noopener noreferrer"
           >
